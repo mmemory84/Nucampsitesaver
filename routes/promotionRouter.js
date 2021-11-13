0b2b231ec3590ec8be42a4a1082d31088a909647
MemoryM <72047128+mmemory84@users.noreply.github.com>
2021-11-12 20:41:27 -0600

    Express REST API with MongoDB and Mongoose Part 1

---
+++ routes/promotionRouter.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const promotionsRouter = express.Router();
+
+// Task 2
+
+promotionsRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the promotions to you');
+})
+.post((req, res) => {
+    res.end(`Will add the promotions: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+})
+.delete((req, res) => {
+    res.end('Deleting all promotions');
+});
+
+promotionsRouter.route('/:promotionId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send details of prmotions: ${req.params.promotionId} to you`);
+})
+.post((req, res) => {
+    res.end(`Will add the promotions: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+})
+.delete((req, res) => {
+    res.end('Deleting all promotions');
+});
+
+module.exports = promotionsRouter;
