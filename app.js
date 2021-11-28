bede378bae376830a5659058091569f66fa27a0e
MemoryM <72047128+mmemory84@users.noreply.github.com>
2021-11-28 09:00:57 -0600

    Week 4 workshop

--- app.js
+++ app.js
@@ -5,26 +5,24 @@ var logger = require('morgan');
 const passport = require('passport');
 const config = require('./config');
 
-
+const mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
-const partnerRouter = require('./routes/partnerRouter');
 const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 const uploadRouter = require('./routes/uploadRouter');
-
-
-const mongoose = require('mongoose');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useFindAndModify: false,
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-connect.then(() => console.log('Connected correctly to server'), 
+connect.then(() => console.log('Connected correctly to server'),
     err => console.log(err)
 );
 
@@ -32,12 +30,12 @@ var app = express();
 
 // Secure traffic only
 app.all('*', (req, res, next) => {
-  if (req.secure) {
-    return next();
-  } else {
-      console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
-      res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
-  }
+    if (req.secure) {
+      return next();
+    } else {
+        console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+        res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+    }
 });
 
 // view engine setup
@@ -49,9 +47,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
 
-
-
-
 app.use(passport.initialize());
 
 app.use('/', indexRouter);
@@ -60,10 +55,10 @@ app.use('/users', usersRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/campsites', campsiteRouter);
-app.use('/partners', partnerRouter);
 app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 app.use('/imageUpload', uploadRouter);
-
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '231312362451372',
+        clientSecret: 'f50b9be72b5790bfeb39e59a5eb7d68d'
+    }
 }
